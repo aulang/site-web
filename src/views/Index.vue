2d66163c3a22bb1413b86e905afcbbf0aea2366a
Aulang <aulang@qq.com>
2022-10-13 19:01:44 +0800

    升级依赖版本

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,33 @@
 <template>
-  <el-button type="primary">登录</el-button>
-  <el-tag>{{ hitokoto }}</el-tag>
+  <div class="flex justify-between m-6">
+    <div class="space-x-4">
+      <el-link type="success">success</el-link>
+      <el-link type="success">success</el-link>
+      <el-link type="success">success</el-link>
+    </div>
+    <div>
+      <el-input v-model="keyword" placeholder="输入关键字搜索">
+        <template #append>.com</template>
+      </el-input>
+    </div>
+  </div>
+
+  <div class="flex justify-center items-center">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <el-button type="primary">登录</el-button>
+        <el-tag>{{ hitokoto }}</el-tag>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { getHitokoto } from '@/api/hitokoto'
 
+const keyword = ref('')
 const hitokoto = ref(null)
 
 onMounted(() => {
@@ -15,4 +37,8 @@ onMounted(() => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-link {
+  font-size: 18px;
+}
+</style>
